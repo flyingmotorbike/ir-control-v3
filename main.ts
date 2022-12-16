eb5fe9af563f1bdfb4811e4abc5ead45d9f1c3f9
Anna Mia Woodward <119895042+flyingmotorbike@users.noreply.github.com>
2022-12-16 19:02:58 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,18 @@
 DFRobotMaqueenPlusV2.onIrDatagram(function (message) {
     basic.showNumber(message)
+    if (message == 0) {
+        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eAllMotor, MyEnumDir.eForward, 50)
+    }
+    if (message == 1) {
+        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eAllMotor, MyEnumDir.eForward, 0)
+    }
+    if (message == 20) {
+        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 50)
+        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, 25)
+    }
+    if (message == 22) {
+        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 25)
+        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 50)
+    }
 })
 DFRobotMaqueenPlusV2.init()
