d50d790d3235dd36febab2a87ea7a19e1f64d676
Anna Mia Woodward <119895042+flyingmotorbike@users.noreply.github.com>
2023-01-22 19:05:25 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,36 @@
+function Turn_Right () {
+    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 50)
+    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, 25)
+}
 DFRobotMaqueenPlusV2.onIrDatagram(function (message) {
     basic.showNumber(message)
-    if (message == 0) {
-        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eAllMotor, MyEnumDir.eForward, 50)
+    if (message == 13) {
+        Move_Forward()
     }
-    if (message == 1) {
-        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eAllMotor, MyEnumDir.eForward, 0)
+    if (message == 6) {
+        Stop()
     }
-    if (message == 20) {
-        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 50)
-        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, 10)
+    if (message == 12) {
+        Turn_Right()
     }
-    if (message == 22) {
-        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 10)
-        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, 50)
+    if (message == 14) {
+        Turn_Left()
     }
-    if (message == 16) {
-        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eAllMotor, MyEnumDir.eBackward, 50)
+    if (message == 17) {
+        Move_Backward()
     }
 })
+function Move_Backward () {
+    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eAllMotor, MyEnumDir.eBackward, 50)
+}
+function Turn_Left () {
+    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 25)
+    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, 50)
+}
+function Move_Forward () {
+    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eAllMotor, MyEnumDir.eForward, 50)
+}
+function Stop () {
+    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eAllMotor, MyEnumDir.eForward, 0)
+}
 DFRobotMaqueenPlusV2.init()
